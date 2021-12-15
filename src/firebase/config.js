9b05fd0c27dfa24cb66c179ace65d1e0861705ab
maan <pranijman@gmail.com>
2021-12-15 10:27:21 +0545

    136

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa1IKHYqFAFGglmudcW8mdOgnq_p_xfdE",
+  authDomain: "muso-ninjas-3a812.firebaseapp.com",
+  projectId: "muso-ninjas-3a812",
+  storageBucket: "muso-ninjas-3a812.appspot.com",
+  messagingSenderId: "138735063603",
+  appId: "1:138735063603:web:4a88e7f6a37d6432b38946"
+};
+
+// init firebase
+firebase.initilizeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
