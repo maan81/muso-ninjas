db85493baed944eb2db3fdf0610df2808b913d3e
maan <pranijman@gmail.com>
2021-12-24 13:15:21 +0545

    163

--- src/components/AddSong.vue
+++ src/components/AddSong.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="add-song">
-    <button v-if="!showForm" @click="showForm = true">Show Form</button>
+    <button v-if="!showForm" @click="showForm = true">Add Songs</button>
     <form v-if="showForm" @submit.prevent="handleSubmit">
       <h4>Add a New Song</h4>
       <input type="text" placeholder="Song Title" required v-model="title">
@@ -12,20 +12,27 @@
 
 <script>
 import { ref } from 'vue';
+import useDocument from '@/composables/useDocument';
 
 export default {
-  setup(){
+  props: ['playlist'],
+  setup(props){
     const title = ref('');
     const artist = ref('');
     const showForm = ref(false);
+    const { updateDoc } = useDocument('playlists', props.playlist.id);
 
     const handleSubmit = async () => {
       const newSong = {
         title: title.value,
         artist: artist.value,
         id: Math.floor(Math.random() * 1000000)
-      }
-      console.log(newSong);
+      };
+      const res = await updateDoc({
+        songs: [...props.playlist.songs, newSong]
+      });
+      title.value = '';
+      artist.value = '';
     };
 
     return { title, artist, showForm, handleSubmit };
@@ -34,11 +41,11 @@ export default {
 </script>
 
 <style scoped>
-  .add-song{
+  .add-song {
     text-align: center;
     margin-top: 40px;
   }
-  form{
+  form {
     max-width: 100%;
     text-align: left;
   }
